e50224f9e7c26064830e9457c67704da1901c002
Walter Nascimento <walter.nascimento.barroso@gmail.com>
2023-04-28 21:29:49 +0100

    💄 UI: Adjust ui for cardBusiness

--- src/pages/BusinessCard/SeeBusinessCardPage.tsx
+++ src/pages/BusinessCard/SeeBusinessCardPage.tsx
@@ -5,10 +5,11 @@ export default () => {
     <div className="flex flex-col items-center justify-center h-screen bg-red-900">
       <BusinessCard
         name="John Doe"
-        title="Front-end Developer"
+        jobTitle="Front-end Developer"
         email="john.doe@example.com"
         phone="(555) 555-5555"
-        address="123 Main St, Anytown ssss"
+        photoUrl="https://avatars.githubusercontent.com/u/8291055?v=4"
+        //  address="123 Main St, Anytown ssss"
       />
     </div>
   );
